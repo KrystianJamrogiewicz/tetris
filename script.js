9ff7c7b1aa3dbec8bee5ec1c7a7d1a760d4f6c55
KrystianJamrogiewicz <krystianjamrogiewicz@gmail.com>
2024-03-25 20:53:28 +0100

    Create DOM, styles, add identifiers from html to js file

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Funkcja zacznie się wykonywać gdy dokument się załaduje
 window.onload = () => {
-	gameArea = document.querySelector;
-	const block = document.createElement("div"); // block będzie tworzyła elementy typu div
-	block.setAttribute("class", "block"); // Dodanie atrybutu class który przyjmie wartość block
-	//document.querySelector(".gameArea").appendChild(block); // Document - plik HTML, querySelector - zwraca pierwszy element w doukmencie pasujący do selektora (klasa gameArea), appendChild - Dodaje element (block) w wcześniej wybranym miejscu (gameArea)
+	for (let i = 0; i < 200; i++) {
+		gameArea = document.querySelector;
+		const block = document.createElement("div"); // block będzie tworzyła elementy typu div
+		block.setAttribute("class", "block"); // Dodanie atrybutu class który przyjmie wartość block
+		document.querySelector(".game-area").appendChild(block); // Document - plik HTML, querySelector - zwraca pierwszy element w doukmencie pasujący do selektora (klasa gameArea), appendChild - Dodaje element (block) w wcześniej wybranym miejscu (gameArea)
+	}
+	const blocks = [...document.querySelectorAll(".block")];
+	console.log(blocks);
+	const scoreDisplay = document.querySelector("#score-display");
+	const startStopBtn = document.querySelector("#btn-start-stop");
 };
