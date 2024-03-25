799c64eaedf75cc6f1b2b48e46ac3e623fe34bb8
KrystianJamrogiewicz <krystianjamrogiewicz@gmail.com>
2024-03-25 18:55:24 +0100

    Create index.html, style.css, script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Funkcja zacznie się wykonywać gdy dokument się załaduje
+window.onload = () => {
+	gameArea = document.querySelector;
+	const block = document.createElement("div"); // block będzie tworzyła elementy typu div
+	block.setAttribute("class", "block"); // Dodanie atrybutu class który przyjmie wartość block
+	//document.querySelector(".gameArea").appendChild(block); // Document - plik HTML, querySelector - zwraca pierwszy element w doukmencie pasujący do selektora (klasa gameArea), appendChild - Dodaje element (block) w wcześniej wybranym miejscu (gameArea)
+};
