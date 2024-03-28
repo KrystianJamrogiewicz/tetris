ab192b9abb1f8ef09c1b6fc6de310bd3fecf341e
KrystianJamrogiewicz <krystianjamrogiewicz@gmail.com>
2024-03-28 12:41:39 +0100

    Create a function that randomizes the block pattern

--- script.js
+++ script.js
@@ -14,6 +14,16 @@ window.onload = () => {
 	const scoreDisplay = document.querySelector("#score-display");
 	const startStopBtn = document.querySelector("#btn-start-stop");
 
+	// Jak czytać numery pól:
+	//     0   1   2   3 ... 9
+	// 0 [00][01][02][03]
+	// 1 [10][11][12][13]
+	// 2 [20][21][22][23]
+	// 3 [30][31][32][33]
+	// ...			      ...
+	// 20            ...[199]
+
+	// Każda tablica przechowywująca nmuery pól dla wyświetlenia bloków w danym kształcie, tablic jest tyle ile możliwych bloków i ich rotacji (gridWidthBlocks domyślnie = 10)
 	const lTetromino = [
 		[1, gridWidthBlocks + 1, gridWidthBlocks * 2 + 1, 2],
 		[
@@ -88,13 +98,25 @@ window.onload = () => {
 		oTetromino,
 		iTetromino,
 	];
-	let currentPosition = 4;
-	let currentBlock = tetrominos[1][3];
+	let currentPosition = 4; // Wybór aktualnej pozycji spawnowania bloków
+	let randomTetromino = Math.floor(Math.random() * tetrominos.length); // Losowanie liczb od 0 do tetrominos.length - 1
+	let randomRotation = Math.floor(Math.random() * 4); // Losowanie liczb od 0 do 3
+
+	let currentBlock = tetrominos[randomTetromino][randomRotation]; // Zmienna przechowująca aktualnie wylosowany wzór bloków
 
 	const drawBlocks = () => {
+		// forEach - Wykona się dla każdego elementu tablicy currentBlock
 		currentBlock.forEach(el => {
-			blocks[currentPosition + el].classList.add("tetrominos");
+			blocks[currentPosition + el].classList.add("tetrominos"); // Dla każdego elementu tablicy currentBlock dodaj currentPosition i klasę tetrominos która zmieni styl elementów w CSS
+		});
+	};
+
+	const removeBlock = () => {
+		currentBlock.forEach(el => {
+			blocks[currentPosition + el].classList.remove("tetrominos");
 		});
 	};
 	drawBlocks();
+	console.log(currentBlock);
+	// removeBlock();
 };
